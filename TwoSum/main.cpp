7a535108744bc4bc0699075aea2e3af1ddddec86
御龙宸 <43564219+LimerZeniYouth@users.noreply.github.com>
2020-01-19 18:03:04 +0800

    Create main.cpp

---
+++ TwoSum/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "TwoSum.h"
+
+using namespace std;
+
+int main() {
+    vector<int> a;
+    vector<int> n = {2, 7, 11, 7};
+    TwoSum twoSum;
+    a = twoSum.twoSum(n, 9);
+    for (int i = 0; i < a.size(); i++) {
+        if (i == 0) {
+            cout << "[" << a[i] << ",";
+        } else if (i+1 == a.size()) {
+            cout << a[i]<<"]";
+        } else {
+            cout << a[i] << ",";
+        }
+    }
+    return 0;
+}
